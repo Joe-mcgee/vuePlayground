2a7e938529a8785328bbf036f4b7de51c9f5bc8a
Taylor <thulsmans.2994@me.com>
2018-05-06 20:59:58 -0600

    added vuex store with getter

--- src/components/Vuex.vue
+++ src/components/Vuex.vue
@@ -1,11 +1,30 @@
 <template>
   <div class='Vuex'>
+    <p>The Safely stored number: {{ doneTodosCount }} </p>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 // https://vuex.vuejs.org/en/intro.html
+// https://alligator.io/vuejs/intro-to-vuex/
 export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
 
+  computed: {
+    ...mapGetters([
+      'doneTodosCount',
+    ]),
+  },
+
+  methods: {
+    increment() {
+      this.count++;
+    },
+  },
 };
 </script>
